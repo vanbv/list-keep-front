1439166a6b61fbc1d7428d44e74e4c4a0b012f88
1ven <van.boinov@gmail.com>
2025-05-23 01:39:05 +0700

    убрал параметры кейклока из адресной строки

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,29 +8,33 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
+import { type Router } from 'vue-router'
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
-})
+// Workaround for https://github.com/keycloak/keycloak/issues/14742
+export default function initRouter (): Router {
+  const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: setupLayouts(routes),
+  })
 
-// Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
+  // Workaround for https://github.com/vitejs/vite/issues/11804
+  router.onError((err, to) => {
+    if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
+      if (!localStorage.getItem('vuetify:dynamic-reload')) {
+        console.log('Reloading page to fix dynamic import error')
+        localStorage.setItem('vuetify:dynamic-reload', 'true')
+        location.assign(to.fullPath)
+      } else {
+        console.error('Dynamic import error, reloading page did not fix it', err)
+      }
     } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
+      console.error(err)
     }
-  } else {
-    console.error(err)
-  }
-})
+  })
 
-router.isReady().then(() => {
-  localStorage.removeItem('vuetify:dynamic-reload')
-})
+  router.isReady().then(() => {
+    localStorage.removeItem('vuetify:dynamic-reload')
+  })
 
-export default router
+  return router
+}
